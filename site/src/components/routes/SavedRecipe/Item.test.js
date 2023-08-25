8db6314a99260d5ff0bcd150742e1ce29ae18b3c
Matt Hackmann <mhackmann13@icloud.com>
2023-08-25 13:00:17 -0500

    Add test for SavedRecipeItem

--- site/src/components/routes/SavedRecipe/Item.test.js
+++ site/src/components/routes/SavedRecipe/Item.test.js
@@ -1,11 +1,12 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 import SavedRecipeItem from "./Item";
 import data from "../../../assets/data.json";
 import Recipe from "../../../utils/Recipe";
 
-test("Save changes button is disabled immediately after switching to form", () => {
+test("Recipe form's save changes button is disabled by default", () => {
   render(
     <BrowserRouter>
       <SavedRecipeItem
@@ -16,4 +17,24 @@ test("Save changes button is disabled immediately after switching to form", () =
   );
 
   expect(screen.getByRole("button", { name: "Save Changes" })).toBeDisabled();
+});
+
+test("Check that calories input value is updating after typing new value", async () => {
+  render(
+    <BrowserRouter>
+      <SavedRecipeItem
+        startRecipe={new Recipe({ ...data[0] })}
+        startingDisplayType="form"
+      />
+    </BrowserRouter>
+  );
+
+  const caloriesInput = screen.getByLabelText("Calories");
+
+  userEvent.clear(caloriesInput);
+  userEvent.type(caloriesInput, "100");
+
+  await waitFor(() => {
+    expect(caloriesInput).toHaveValue(100);
+  });
 });
