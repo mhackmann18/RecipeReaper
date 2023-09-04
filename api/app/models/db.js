75e582fdaa3ec54bb87beb58d627589e4569462f
Matt Hackmann <mhackmann13@icloud.com>
2023-09-04 13:02:24 -0500

    remove port from mysql connection

--- api/app/models/db.js
+++ api/app/models/db.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 module.exports = async function () {
   const conn = await mysql.createConnection({
     host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
+    // port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password:
       process.env.MYSQL_PASSWORD ||
