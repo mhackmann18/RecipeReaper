b9cff1c239cd62d049911fe3b9f8ff34acb01f22
Matt Hackmann <mhackmann13@icloud.com>
2023-09-04 12:43:29 -0500

    test

--- api/server.js
+++ api/server.js
@@ -37,6 +37,9 @@ require("./app/routes/user")(app);
 
 const { PORT, NODE_ENV } = process.env;
 
+const MAX_RETRY_ATTEMPTS = 5;
+const RETRY_INTERVAL = 5000;
+
 if (NODE_ENV === "production") {
   const options = {
     key: fs.readFileSync("/etc/pki/tls/private/rrapi-tls.key"),
@@ -53,13 +56,44 @@ if (NODE_ENV === "production") {
 }
 
 if (NODE_ENV === "development") {
-  try {
-    init().then(() => {
+  // try {
+  //   init().then(() => {
+  //     app.listen(PORT, () => {
+  //       console.log(`Server is running on port ${PORT}.`);
+  //     });
+  //   });
+  // } catch (err) {
+  //   console.log(err.red);
+  // }
+  // eslint-disable-next-line no-use-before-define
+  connectToDBWithRetry(MAX_RETRY_ATTEMPTS, RETRY_INTERVAL);
+}
+
+async function connectToDBWithRetry(maxAttempts, interval) {
+  let retryCount = 0;
+
+  async function tryConnect() {
+    try {
+      await init();
+      console.log("Database connected successfully!");
       app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}.`);
       });
-    });
-  } catch (err) {
-    console.log(err.red);
+    } catch (err) {
+      console.error(
+        `Error connecting to the database (Attempt ${retryCount + 1}):`,
+        err
+      );
+      retryCount++;
+
+      if (retryCount < maxAttempts) {
+        setTimeout(tryConnect, interval);
+      } else {
+        console.error(`Max retry attempts (${maxAttempts}) reached. Exiting.`);
+        process.exit(1); // Exit the application if max retry attempts are reached
+      }
+    }
   }
+
+  tryConnect();
 }
