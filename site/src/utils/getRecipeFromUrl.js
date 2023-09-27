9bf1a68826adec02a600e772381e6557b3b7d57b
Matt Hackmann <mhackmann13@icloud.com>
2023-09-27 16:35:07 -0500

    RSF support recipe links copied from google in safari

--- site/src/utils/getRecipeFromUrl.js
+++ site/src/utils/getRecipeFromUrl.js
@@ -3,11 +3,25 @@ import Recipe from "./Recipe";
 
 // Return Recipe or error string
 export default async function getRecipeFromUrl(url) {
+  // In the Safari browser, copying a link from google's search results does not copy the actual site url.
+  // Instead the clipboard value is google.com's url with the site url passed as a query parameter.
+  // Here, a regex is used to check if the url is in the aformentioned format, and if so, the url query param is extracted.
+  const matches = url.match(/^https:\/\/www\.google\.com\/url\?.+/);
+
+  let parsedUrl = url;
+
+  if (matches) {
+    const urlParamValue = getUrlQueryParamValue(url);
+
+    // Url param value typically must be decoded
+    parsedUrl = decodeURIComponent(urlParamValue);
+  }
+
   let response;
 
   try {
     response = await fetch(
-      `${process.env.REACT_APP_SCRAPER_ORIGIN}/recipe-data?url=${url}`
+      `${process.env.REACT_APP_SCRAPER_ORIGIN}/recipe-data?url=${parsedUrl}`
     );
   } catch (error) {
     return error;
@@ -51,4 +65,17 @@ export default async function getRecipeFromUrl(url) {
 
   const errText = await response.text();
   return errText;
+}
+
+function getUrlQueryParamValue(url) {
+  // Match url variable name and value
+  const matches = url.match(/url=https.+?(?=&)/);
+
+  if (matches) {
+    // Only return url variable's value
+    const result = matches[0].replace(/url=/, "");
+    return result;
+  }
+
+  return null;
 }
