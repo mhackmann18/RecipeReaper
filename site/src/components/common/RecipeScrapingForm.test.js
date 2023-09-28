64aca7a3f28073f02781f8fb9c5d792740ea0223
Matt Hackmann <mhackmann13@icloud.com>
2023-09-27 19:48:47 -0500

    write render test for RSF

--- site/src/components/common/RecipeScrapingForm.test.js
+++ site/src/components/common/RecipeScrapingForm.test.js
@@ -1,17 +1,24 @@
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import RecipeScrapingForm from "./RecipeScrapingForm";
 
 test("Renders text input and button", () => {
   const fn = () => null;
 
-  // screen.
-
   render(
     <BrowserRouter>
-      <RecipeScrapingForm onSubmit={fn} onSuccess={fn} onFailure={fn} />
+      <RecipeScrapingForm onSubmit={fn} onSuccess={fn} />
     </BrowserRouter>
   );
+
+  expect(
+    screen.getByRole("button", { name: /Get Recipe/i })
+  ).toBeInTheDocument();
+
+  expect(
+    screen.getByPlaceholderText(/Paste a recipe's url/i)
+  ).toBeInTheDocument();
 });
 
 test("Show error message after submitting incorrectly formatted url input", () => {
