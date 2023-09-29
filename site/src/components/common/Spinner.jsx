15292acf9a4f31169c8fcdcbdc798d220b04ab65
Matt Hackmann <mhackmann13@icloud.com>
2023-09-29 16:55:48 -0500

    lessen spinner's default width

--- site/src/components/common/Spinner.jsx
+++ site/src/components/common/Spinner.jsx
@@ -1,5 +1,6 @@
 import spinner from "../../assets/loading-gif.gif";
+import "./Spinner.css";
 
 export default function Spinner() {
-  return <img id="rsf-spinner" src={spinner} alt="spinner-gif" />;
+  return <img className="spinner" src={spinner} alt="spinner-gif" />;
 }
